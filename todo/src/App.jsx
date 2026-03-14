a333c98e5aab0fb736cea16a13ec2691752657c1
pranali <kokatepranu833@gmail.com>
2026-03-14 21:13:49 +0530

    Update App.jsx
    changes added in app code

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,165 +1,41 @@
-import React from 'react'
-import '../src/App.css'
-import data from '../src/data.js'
-import { useState } from 'react';
-import { ArrowBigRight } from 'lucide-react';
-import { ArrowBigLeft } from 'lucide-react';
-import toast,{ Toaster } from 'react-hot-toast';
-
-
-const startIndex = 0;
-const endIndex = data.length - 5;
-const randomIndex = Math.floor(Math.random() * (endIndex - startIndex + 1)) + startIndex;
-const quizData = data.slice(randomIndex, randomIndex + 5);
-
+import {useEffect, useState} from 'react';
+import TaskCard from './TaskCard.js';
+import NotFound from './assets/NotFound.png';
 
 function App() {
+const [tasks, setTasks] = useState([]);
+const [newTak , setNewTask] = useState("");
 
-  const [quetionIndex, setQuestionIndex] = useState(0);
-
-  const [optionStyle, setOptionStyle] = useState({
-    0:{},
-    1:{},
-    2:{},
-    3:{}, 
-    4:{},
-  });
-
-
-  const currentQuetion = quizData[quetionIndex];
-
-
-  const checkAnswer = (selectedOption, idx) => {
-
-    if(selectedOption === currentQuetion.answer){
-
-      toast.success("Correct Answer!");
-
-      setOptionStyle({
-        ...optionStyle,
-        [idx]:{
-          backgroundColor:"green",
-          color:"white"
-        }
-      });
-
-    }else{
-
-      toast.error(`Wrong Answer! The correct answer is: ${currentQuetion.answer}`);
-
-      setOptionStyle({
-        ...optionStyle,
-        [idx]:{
-          backgroundColor:"red",
-          color:"white"
-        }
-      });
-
-    }
-
+useEffect(() => {
+  const savedTasks = JSON.parse(localStorage.getItem("tasks")); 
+  if (savedTasks) {
+    setTasks(savedTasks);
   }
+}, []);
+const savedTasks = (tasks) =>{
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+ const addtask=()=>{
+  if(newTak.trim())return;
+  const updatedTasks = [...tasks, newTak];
+  setTasks(updatedTasks);
+  savedTasks(updatedTasks);
+   setNewTask("");
+ };
+ constdeleteTask=(taskName)=>{
+  const filteredTasks = tasks.filter((t)=> t !== taskName);
+  setTasks(filteredTasks);
+  savedTasksLocalStorage(filteredTasks);
+ };
+
+ return(
+  <div className='min-h-screen bg-[#f5f6fa] flex flex-col justify-center px-4 py-8'>
+    <h2 className='text-center text-3xl font-bold text-[#333] mb-6 max-sm:text-[26px]'>Todo App</h2>
+    <p className='text-center text-[#6c6c8a] mb-6 max-sm:text-[14px] max-sm:mb-[18px]'>Add your tasks </p>
+ <div className='p-5 w-[480px] h-[50vh] overflow-y-auto max-auto mb-5 rounded-2xl bg-white
+ shadow-[0_12px_30px_rgba(0,0,0,0.2)]
+ max-sm:w-full max-sm:h-[55vh] max-sm:p-4 max-sm:rounded-[14px]'>
+ );
 
 
-  return (
-    <>
-
-    <h1 className='app-heading'>Quiz App</h1> 
-
-    <div className="App">
-
-      <p className='text-quetion-number'>
-        Question Number: {quetionIndex + 1} / {quizData.length}
-      </p>
-
-
-      <p className='quetion'>
-        {currentQuetion.question}
-      </p>
-
-
-      {
-        currentQuetion.options.map((option, idx)=>{
-
-          return(
-
-            <div
-              key={idx}
-              className='option-card'
-              onClick={()=> checkAnswer(option, idx)}
-              style={optionStyle[idx]}
-            >
-              {option}
-            </div>
-
-          )
-
-        })
-      }
-
-
-      <div className='arrow'>
-
-
-        <ArrowBigLeft
-          size={40}
-          className='img-prev-quetion'
-          onClick={() => {
-
-            if(quetionIndex > 0){
-
-              setQuestionIndex(quetionIndex - 1);
-
-              setOptionStyle({
-                0:{},
-                1:{},
-                2:{},
-                3:{}, 
-                4:{},
-              });
-
-            }
-
-          }}
-        />
-
-
-        <ArrowBigRight
-          size={40}
-          className='img-next-quetion'
-          onClick={() => {
-
-            if(quetionIndex < quizData.length - 1){
-
-              setQuestionIndex(quetionIndex + 1);
-
-              setOptionStyle({
-                0:{},
-                1:{},
-                2:{},
-                3:{}, 
-                4:{},
-              });
-
-            }else{
-
-              toast.success("Quiz Completed 🎉");
-
-            }
-
-          }}
-        />
-
-
-      </div>
-
-
-      <Toaster position="top-center"/>
-
-    </div>
-
-    </>
-  )
-
-}
-
-export default App
+  export default App;
